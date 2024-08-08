69391f2e4795c06d0f142d8979cdfac07288a04d
mrkvkrl <mrkvkrl@yandex.ru>
2024-08-09 00:45:32 +0300

    successful run

--- AddGroup.py
+++ AddGroup.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class AddGroup(unittest.TestCase):
     def setUp(self):
@@ -14,13 +14,21 @@ class AddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        wd.find_element(By.LINK_TEXT,  "groups").click()
+
+        self.Open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.create_new_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "group page").click()
+
+    def create_new_group(self, wd):
         wd.find_element(By.NAME, "new").click()
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
@@ -32,9 +40,20 @@ class AddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys("something else")
         wd.find_element(By.NAME, "submit").click()
-        wd.find_element(By.LINK_TEXT, "group page").click()
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-    
+
+    def open_group_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, wd):
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def Open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
