fdc44681d1f97ecb7a749190517e7b281487397e
mrkvkrl <mrkvkrl@yandex.ru>
2024-08-09 03:31:24 +0300

    add class

--- AddNewContact.py
+++ AddNewContact.py
@@ -5,7 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from contact import Contact
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
@@ -15,10 +16,10 @@ class AddNewContact(unittest.TestCase):
     def test_add_new_contact(self):
         wd = self.wd
         self.open_addressbook(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_add_contact_page(wd)
-        self.create_contact(wd, name="petr", middle_name="petrovich", last_name="petrov", nickname="ptr", company="yandex", address="minin strasse", home="587676586896",
-                            phone="56253687263", work="2342341323", email="petr@yandex.ru")
+        self.create_contact(wd, Contact(name="petr", middle_name="petrovich", last_name="petrov", nickname="ptr", company="yandex", address="minin strasse", home="587676586896",
+                            phone="56253687263", work="2342341323", email="petr@yandex.ru"))
         self.save_contact(wd)
         self.go_to_homepage(wd)
         self.logout(wd)
@@ -32,37 +33,37 @@ class AddNewContact(unittest.TestCase):
     def save_contact(self, wd):
         wd.find_element(By.XPATH, "//div[@id='content']/form/input[20]").click()
 
-    def create_contact(self, wd, name, middle_name, last_name, nick, company, address, home, mobile, work, email):
+    def create_contact(self, wd,contact):
         wd.find_element(By.NAME, "firstname").click()
-        wd.find_element(By.NAME, "firstname").send_keys(name)
+        wd.find_element(By.NAME, "firstname").send_keys(contact.name)
         wd.find_element(By.NAME, "middlename").click()
-        wd.find_element(By.NAME, "middlename").send_keys(middle_name)
+        wd.find_element(By.NAME, "middlename").send_keys(contact.middle_name)
         wd.find_element(By.NAME, "lastname").click()
-        wd.find_element(By.NAME, "lastname").send_keys(last_name)
+        wd.find_element(By.NAME, "lastname").send_keys(contact.last_name)
         wd.find_element(By.NAME, "nickname").click()
-        wd.find_element(By.NAME, "nickname").send_keys(nick)
+        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
         wd.find_element(By.NAME, "company").click()
-        wd.find_element(By.NAME, "company").send_keys(company)
+        wd.find_element(By.NAME, "company").send_keys(contact.company)
         wd.find_element(By.NAME, "address").click()
         wd.find_element(By.NAME, "address").click()
         wd.find_element(By.NAME, "address").click()
         wd.find_element(By.NAME, "address").click()
-        wd.find_element(By.NAME, "address").send_keys(address)
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
         wd.find_element(By.NAME, "home").click()
-        wd.find_element(By.NAME, "home").send_keys(home)
+        wd.find_element(By.NAME, "home").send_keys(contact.home)
         wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").send_keys(mobile)
+        wd.find_element(By.NAME, "mobile").send_keys(contact.phone)
         wd.find_element(By.NAME, "work").click()
-        wd.find_element(By.NAME, "work").send_keys(work)
+        wd.find_element(By.NAME, "work").send_keys(contact.work)
         wd.find_element(By.NAME, "email").click()
-        wd.find_element(By.NAME, "email").send_keys(email)
+        wd.find_element(By.NAME, "email").send_keys(contact.email)
 
     def open_add_contact_page(self, wd):
         wd.find_element(By.LINK_TEXT, "add new").click()
 
-    def login(self, wd):
-        wd.find_element(By.NAME, "user").send_keys("admin")
-        wd.find_element(By.NAME, "pass").send_keys("secret")
+    def login(self, wd, username, password):
+        wd.find_element(By.NAME, "user").send_keys(username)
+        wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
     def open_addressbook(self, wd):
