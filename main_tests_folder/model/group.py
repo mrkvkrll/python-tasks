27f9de6f485e0a669a3aa29610a640fa0c57ffbe
mrkvkrl <mrkvkrl@yandex.ru>
2024-08-17 01:56:59 +0300

    ctreated packages

---
+++ main_tests_folder/model/group.py
@@ -0,0 +1,6 @@
+class Group:
+
+    def __init__(self, name, description, other):
+        self.name = name
+        self.description = description
+        self.other = other
---
+++ main_tests_folder/test/AddGroup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from main_tests_folder.model.group import Group
+from main_tests_folder.fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.open_group_page()
+    app.create_new_group(Group(name="first_group", description="description", other="something else"))
+    app.return_to_group_page()
+    app.logout()
+
+def test_add_empty_group(app):
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.open_group_page()
+    app.create_new_group(Group(name="", description="", other=""))
+    app.return_to_group_page()
+    app.logout()
